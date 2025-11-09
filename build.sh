d9bd962088d90888af1e918f36576a76d194b77a
DiCesareDaniele <dicesaredaniele7@gmail.com>
2025-11-09 01:44:08 +0100

    name

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-readarray -t lang_files < <(find langs -maxdepth 1 -type f -name "*.toml")
-
+name="DiCesare_Daniele"
 input_file="main.typ"
 output_dir="build"
+
+readarray -t lang_files < <(find langs -maxdepth 1 -type f -name "*.toml")
 
 mkdir -p "$output_dir"
 
 for lang_file in "${lang_files[@]}"; do
     lang="${lang_file##*/}"
     lang="${lang%.toml}"
-    output_file="$output_dir/cv-$lang.pdf"
+    lang="${lang^^}"
+    output_file="$output_dir/$name-$lang.pdf"
     typst compile "$input_file" --input lang-file="$lang_file" "$output_file"
 done
